6193eae9205727008eab8597b26aea095a5149e6
jovemdan <danilogdev@gmail.com>
2022-08-17 21:44:05 -0300

    feat: Colocando o Link para redirecionamento

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -5,6 +5,7 @@ import styles from "./styles.module.scss";
 import { RichText } from "prismic-dom";
 
 import Prismic from "@prismicio/client";
+import Link from "next/link";
 
 type Post = {
   slug: string;
@@ -26,11 +27,13 @@ export default function Posts({ posts }: PostsProps) {
       <main className={styles.cointainer}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href='#'>
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link href={`/posts/${post.slug}`}>
+              <a key={post.slug}>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
